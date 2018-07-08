7bd9f9b05694a8985421cbcc00003af72f4ed7ce
Branden <b.lowe333@gmail.com>
2018-07-08 13:49:27 -0700

    passings steps problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,48 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// Option 3 - pure recursive
+function steps(n, row=0, stair='') {
+  if (row === n) return // recursive base-case
+  if (stair.length < n) {
+    stair += stair.length <= row ? '#' : ' '
+    steps(n, row, stair)
+  } else {
+    console.log(stair)
+    return steps(n, row + 1)
+  }
+}
+
 
 module.exports = steps;
+
+// Option 2: recursive
+// function steps(n, row=0) {
+//   if (row === n) return // recursive base-case
+//   let line = ''
+
+//   for (let i = 0; i < n; i++) {
+//     line += i <= row ? '#' : ' '
+//   }
+//   console.log(line)
+//   steps(n, ++row)
+// }
+
+
+// Option 1
+// function steps(n) {
+//   for (let i = 0; i < n; i++) {
+//     let line = ''
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) { 
+//         line += '#'
+//       } else {
+//         line += ' '
+//       }
+//     }
+//     // while (line.length < n) {
+//     //   line += ' '
+//     // }
+//     console.log(line)
+//   }
+// }
