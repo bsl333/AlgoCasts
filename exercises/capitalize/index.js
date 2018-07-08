3af45a939de259307ddba7cb07b0a086ad681775
Branden <b.lowe333@gmail.com>
2018-07-08 11:22:48 -0700

    passing capitalize tests

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,24 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+//option 2 - maintain proper spacing in string
+function capitalize(str) {
+  const words = str.match(/[^\s]+/g) // could use /\w+/g
+  const spaces = str.match(/\s+/g)
+  return words
+    .map(val => val[0].toUpperCase() + val.slice(1))
+    .reduce((accum, val, idx) => {
+      return spaces[idx] ? accum + val + spaces[idx] : accum + val
+    }, '')
+  
+}
 
 module.exports = capitalize;
+
+// Option 1
+// function capitalize(str) {
+//   return str
+//     .split(' ')
+//     .map(val => val[0].toUpperCase() + val.slice(1))
+//     .join(' ')
+// }
