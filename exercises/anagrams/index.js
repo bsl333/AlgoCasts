13a5175cc29dd1c8518d8fa96af5b5ab079b71fe
Branden <b.lowe333@gmail.com>
2018-07-07 18:21:20 -0700

    passing anagrams test

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+//option 1
+function anagrams(stringA, stringB) {
+  const regEx = /[a-z]/gi
+  const stringArrayA = stringA.toLowerCase().match(regEx)
+  const stringArrayB = stringB.toLowerCase().match(regEx)
+  // return JSON.stringify(stringArrayA.sort()) === JSON.stringify(stringArrayB.sort())
+  return stringArrayA.sort().join('') === stringArrayB.sort().join('')
+}
 
 module.exports = anagrams;
+
+
+//option 1
+// function anagrams(stringA, stringB) {
+//   const regEx = /[a-z]/gi
+//   // console.log(stringA.match(/\w+/g))
+//   const newStringA = stringA.match(regEx).join('').toLowerCase()
+//   const newStringB = stringB.match(regEx).join('').toLowerCase()
+//   if (newStringA.length !== newStringB.length) return false
+//   const stringAObj = {}
+//   const stringBObj = {}
+//   for (let idx in newStringA) {
+//     const charA = newStringA[idx]
+//     const charB = newStringB[idx]
+//     stringAObj[charA] = ++stringA[charA] || 1
+//     stringBObj[charB] = ++stringB[charB] || 1
+//   }
+//   for (let char in stringAObj) {
+//     if (stringAObj[char] !== stringBObj[char]) return false
+//   }
+//   return true
+// }
