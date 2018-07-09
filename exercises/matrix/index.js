bc35ae27b57684a9706b8027848e23627ebc5aa5
Branden <b.lowe333@gmail.com>
2018-07-09 12:30:54 -0700

    passing spiral test

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  // initialize grid
+  const matrix = []
+  while (matrix.length < n) matrix.push([]) 
+  let startRow = 0,
+      endRow = n - 1,
+      startColumn = 0,
+      endColumn = n - 1,
+      count = 1
+
+  while (count <= n * n) {
+    // create top row
+    for (let i = startColumn; i <= endColumn; i++) {
+      matrix[startRow][i] = count++
+    }
+    startRow++
+
+    // create right side
+    for (let i = startRow; i <= endRow; i++) {
+      matrix[i][endColumn] = count++
+    }
+    endColumn--
+
+    // create bottom
+    for (let i = endColumn; i >= startColumn; i--) {
+      matrix[endRow][i] = count++
+    }
+    endRow--
+
+    // create leftside
+    for (let i = endRow; i >= startRow; i--) {
+      matrix[i][startColumn] = count++
+    }
+    startColumn++
+  }
+  
+  // matrix.forEach(val => console.log(val));
+  return matrix
+  
+}
+
 
 module.exports = matrix;
